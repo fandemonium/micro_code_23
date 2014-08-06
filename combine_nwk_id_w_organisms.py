78e7fb4a2af749c5f76f1b47ad6c625e37d74a98
FY <snisiarc@gmail.com>
2014-08-06 00:17:30 -0500

    added another code for adding organims to nwk id

---
+++ combine_nwk_id_w_organisms.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from Bio import SeqIO
+import re
+
+l_id = []
+l_long_id = []
+
+s = open(sys.argv[1], 'r')
+for line in SeqIO.parse(s, 'fasta'):
+    name, seq = line.id, line.seq.tostring()
+    l_long_id.append(re.split("\|", name)[2])
+    l_id.append(re.split("\|", name)[1])
+#print l_seq
+# '|' itself is a nulti-divider seperater. need \ to escape it to be used as a divider itself.
+#print len(l_id)
+dict_id = dict(zip(l_id, l_long_id))
+
+id_to_grab = []
+with open(sys.argv[2]) as f:
+    next(f)
+    for line in f:
+        lexemes = re.split("\t|\n", line)
+        gene_id = lexemes[1]
+        id_to_grab.append(gene_id)
+
+out = open(sys.argv[3], 'w')
+for item in id_to_grab:
+    out.write('%s\t%s\n' % (item,dict_id[item]))
