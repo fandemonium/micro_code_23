7b903ffd8a53c171b3b997f760de787ab9f4a794
FY <snisiarc@gmail.com>
2014-08-19 11:14:40 -0500

    modified for space delimited id list

--- combine_nwk_id_w_organisms.py
+++ combine_nwk_id_w_organisms.py
@@ -11,7 +11,7 @@ for line in s:
     if line.startswith(">"):
         l_long_id.append(re.split("\||=", line)[3])
         l_id.append(re.split("\|", line)[1])
-#print l_long_id
+#	print re.split("\||=", line)[3]
 # '|' itself is a nulti-divider seperater. need \ to escape it to be used as a divider itself.
 #print len(l_id)
 dict_id = dict(zip(l_id, l_long_id))
@@ -20,10 +20,10 @@ id_to_grab = []
 with open(sys.argv[2]) as f:
     next(f)
     for line in f:
-        lexemes = re.split("\t|\n", line)
+        lexemes = re.split(" |_|\n", line)
         gene_id = lexemes[1]
         id_to_grab.append(gene_id)
-
+#	print gene_id
 out = open(sys.argv[3], 'w')
 for item in id_to_grab:
     out.write('%s\t%s\n' % (item,dict_id[item]))
